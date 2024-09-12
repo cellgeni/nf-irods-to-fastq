c5bcc33a5d16d2ae6f34bca639dfff93813897da
Aljes <aleksandr.binkevich@gmail.com>
2024-09-12 15:07:56 +0100

    Formated scripts

--- bin/combine_meta.py
+++ bin/combine_meta.py
@@ -7,22 +7,28 @@ import csv
 import re
 
 
-def make_unique(sample_fastq_name: str, unique_tags: set[int], fastq_names: list[str]) -> str:
+def make_unique(
+    sample_fastq_name: str, unique_tags: set[int], fastq_names: list[str]
+) -> str:
     """
     Creates a unique filename
     sample_fastq_name (str): Current fastq file name
     unique_tags (set[int]): A set of tags that are available for use
-    fastq_names (list[str]): A list of fastq_names that are currently in use 
+    fastq_names (list[str]): A list of fastq_names that are currently in use
     """
     # get tag id from fastq file name
-    sample_tag = re.search('_S(\d+)_', sample_fastq_name).group(1)
+    sample_tag = re.search("_S(\d+)_", sample_fastq_name).group(1)
 
     # check if filename is already in use
     if sample_fastq_name in fastq_names:
-        unique_tag = unique_tags.pop() 
-        sample_fastq_name = sample_fastq_name.replace(f'_S{sample_tag}_', f'_S{unique_tag}_')
-        print(f"Filename changed from {sample_fastq_name.replace(f'_S{unique_tag}_', f'_S{sample_tag}_')} to {sample_fastq_name}")
-    
+        unique_tag = unique_tags.pop()
+        sample_fastq_name = sample_fastq_name.replace(
+            f"_S{sample_tag}_", f"_S{unique_tag}_"
+        )
+        print(
+            f"Filename changed from {sample_fastq_name.replace(f'_S{unique_tag}_', f'_S{sample_tag}_')} to {sample_fastq_name}"
+        )
+
     # add filename to the list and remove sample_tag from taglist
     fastq_names.append(sample_fastq_name)
     unique_tags = unique_tags - {int(sample_tag)}
@@ -31,8 +37,8 @@ def make_unique(sample_fastq_name: str, unique_tags: set[int], fastq_names: list
 
 def main():
     # read positional argument with filedir path
-    dirpath = sys.argv[1].strip('/')
-    number_of_samples = len(os.listdir('./input/'))
+    dirpath = sys.argv[1].strip("/")
+    number_of_samples = len(os.listdir("./input/"))
 
     # read all json files to meta_list
     meta_list = list()
@@ -40,21 +46,23 @@ def main():
     unique_tags = set(range(1, number_of_samples))
 
     for filename in os.listdir(dirpath):
-        with open(f'{dirpath}/{filename}', 'r') as file:
+        with open(f"{dirpath}/{filename}", "r") as file:
             # reading the json file
             sample_meta = json.load(file)
             # making fastq_name unique
-            sample_meta['fastq_name'] = make_unique(sample_meta['fastq_name'], unique_tags, fastq_names)
+            sample_meta["fastq_name"] = make_unique(
+                sample_meta["fastq_name"], unique_tags, fastq_names
+            )
             meta_list.append(sample_meta)
 
     # save the field names
     fieldnames = sample_meta.keys()
 
-    # sort the the data by sample name 
-    meta_list = sorted(meta_list, key=lambda x: x['sample'])
+    # sort the the data by sample name
+    meta_list = sorted(meta_list, key=lambda x: x["sample"])
 
     # write all metadata to csv
-    with open('metadata.csv', mode='w') as csv_file:
+    with open("metadata.csv", mode="w") as csv_file:
         # create writer object
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
 
@@ -64,5 +72,5 @@ def main():
             writer.writerow(sample_meta)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
